2b458b7ebf1b5db383897bae9fb82248215598d0
Strannik213123 <eyyubalizade15@gmail.com>
2025-12-04 17:31:43 +0400

    ewe

--- pss.js
+++ pss.js
@@ -1,4 +1,5 @@
-let userName = "Samir";
-let surname = "Aliyev";
-let fullName=userName+""+surname
-console.log( fullname+ userName + " " + surname + ".");
+let number= prompt("Dord regem yazin");
+console.log(number);
+ let username= prompt("Adinizi yazin");
+console.log( "salam" + ""  + username);
+
