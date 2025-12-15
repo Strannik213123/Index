a727da23c91bcb594754fceef83dc42c4671ebff
Strannik213123 <eyyubalizade15@gmail.com>
2025-12-15 13:53:13 +0400

    Ll

--- pss.js
+++ pss.js
@@ -1,17 +1,22 @@
-let input = prompt("Tarixi 'YYYY.MM.DD daxil edin:")
-let [year, month, day] = input.split(".")
-let months  = ["Yanvar", "Fevral", "Mart", "Aprel", "May", "Iyun", "Iyul", "Avqust", "Sentyabr", "Oktyabr", "Noyabr", "Dekabr"]
-let monthName = months[parseInt(month) - 1]
-console.log(`${day} ${monthName} ${year} `)
-let result = '${parseInt(day)} ${monthName} $(year)}-cu il';
-alert(result);
-let monthNumber = prompt("Ayin nomresini daxil edin (1-12):");
-let months = [
-    "Yanvar", "Fevral", "Mart", "Aprel", "May", "Iyun", "Iyul", "Avqust", "Sentyabr", "Oktyabr", "Noyabr", "Dekabr"
-]
-let index = parseInt(monthNumber) - 1;
-if (index >= 0 && index < 12) {
-    console.log(`Ayin adi: ${months[index]}`);
-} else {
-    console.log("Daxil edilen nomre yanlisdir. Zəhmət olmasa 1-dən 12-ə qeder olan bir nomre daxil edin.");
+
+capitalizeWords("hello world from pss.js");
+function capitalizeWords(str) {
+    return str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
 }
+console.log(capitalizeWords("hello world from pss.js")); 
+numCounter([1, 2, 2, 3, 4, 4, 4, 5]);
+function numCounter(arr) {
+    const count = {};   
+    arr.forEach(num => {
+        count[num] = (count[num] || 0) + 1;
+    }
+    );
+    return count;
+} 
+console.log(numCounter([1, 2, 2, 3, 4, 4, 4, 5]));
+isvalidnumber("+7123-456-78902");
+function isvalidnumber(phone) {
+    const regex = /^\+7\d{3}-\d{3}-\d{5}$/;
+    return regex.test(phone);
+}
+console.log(isvalidnumber("+7123-456-78902"));
