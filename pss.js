96ddb6e0d9437d7825b854413c90f4bfc9c3c7f8
Strannik213123 <eyyubalizade15@gmail.com>
2025-12-18 19:09:36 +0400

    f

--- pss.js
+++ pss.js
@@ -1,22 +1,14 @@
-
-capitalizeWords("hello world from pss.js");
-function capitalizeWords(str) {
-    return str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+let a = 100
+while (a <= 990) {
+console.log(a);
+a += 10;
 }
-console.log(capitalizeWords("hello world from pss.js")); 
-numCounter([1, 2, 2, 3, 4, 4, 4, 5]);
-function numCounter(arr) {
-    const count = {};   
-    arr.forEach(num => {
-        count[num] = (count[num] || 0) + 1;
-    }
-    );
-    return count;
-} 
-console.log(numCounter([1, 2, 2, 3, 4, 4, 4, 5]));
-isvalidnumber("+7123-456-78902");
-function isvalidnumber(phone) {
-    const regex = /^\+7\d{3}-\d{3}-\d{5}$/;
-    return regex.test(phone);
+console.log("Task 2")
+let sum 0
+let b = 11
+while (b <= 99) {
+// console.log(b)
+sum += b;
+b += 2;
 }
-console.log(isvalidnumber("+7123-456-78902"));
+console.log(sum)
