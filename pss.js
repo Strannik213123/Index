88695df434c7f05690118618336422a85362a3ae
Strannik213123 <eyyubalizade15@gmail.com>
2025-12-29 19:14:50 +0400

    вв

--- pss.js
+++ pss.js
@@ -1,19 +1,9 @@
-let arr [7,3,8,9,2,1,4]);
-
-let newArray = []
-
-for (let 1 = 0; i < arr.length; i++) {
-
-if (arr[1] > 5) {
-
-newArray.push(arr[i])
-
-}else{
-
-newArray.unshift(arr[1])
-
+let arr = [1, 2, 3, 4, 5];
+let newArr = [...arr];
+for (let i=0; i< newArr.length; i++) {
+newArr[1] 2
+6
 }
-
-}
-
-console.log(newArray)
+console.log(arr);
+8
+console.log(newArr);
